b1214c4a490b1ac608002f75711ac01006d299e0
Jonathan Tay <jonathantaymy@gmail.com>
2024-01-25 11:37:27 +0800

    style: format with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@
 const config = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: true
+        project: true,
     },
     plugins: ["@typescript-eslint"],
     extends: [
         "next/core-web-vitals",
         "plugin:@typescript-eslint/strict-type-checked",
-        "plugin:@typescript-eslint/stylistic-type-checked"
+        "plugin:@typescript-eslint/stylistic-type-checked",
     ],
     rules: {
         "@typescript-eslint/array-type": "off",
@@ -16,40 +16,40 @@ const config = {
         "@typescript-eslint/no-confusing-void-expression": [
             "error",
             {
-                ignoreArrowShorthand: true
-            }
+                ignoreArrowShorthand: true,
+            },
         ],
         "react/no-unescaped-entities": "off",
         "@typescript-eslint/consistent-type-imports": [
             "error",
             {
                 prefer: "type-imports",
-                fixStyle: "inline-type-imports"
-            }
+                fixStyle: "inline-type-imports",
+            },
         ],
         "@typescript-eslint/no-misused-promises": [
             "error",
             {
                 checksVoidReturn: {
-                    attributes: false
-                }
-            }
+                    attributes: false,
+                },
+            },
         ],
         "@typescript-eslint/restrict-template-expressions": [
             "warn",
             {
-                allowBoolean: true
-            }
+                allowBoolean: true,
+            },
         ],
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unused-vars": [
             "error",
             {
                 varsIgnorePattern: "^_",
-                argsIgnorePattern: "^_"
-            }
-        ]
-    }
+                argsIgnorePattern: "^_",
+            },
+        ],
+    },
 };
 
 module.exports = config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type {Config} from "tailwindcss"
+import type { Config } from "tailwindcss";
 import tailwindAnimate from "tailwindcss-animate";
 import tailwindContainerQueries from "@tailwindcss/container-queries";
 
 const config = {
     darkMode: ["class"],
     content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}',
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
     ],
     prefix: "",
     theme: {
@@ -62,12 +62,12 @@ const config = {
             },
             keyframes: {
                 "accordion-down": {
-                    from: {height: "0"},
-                    to: {height: "var(--radix-accordion-content-height)"},
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
                 },
                 "accordion-up": {
-                    from: {height: "var(--radix-accordion-content-height)"},
-                    to: {height: "0"},
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
                 },
             },
             animation: {
@@ -84,6 +84,6 @@ const config = {
         },
     },
     plugins: [tailwindAnimate, tailwindContainerQueries],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
