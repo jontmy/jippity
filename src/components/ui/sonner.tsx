da417de047551d4c5b6add171e2aba3766c42374
Jonathan Tay <jonathantaymy@gmail.com>
2024-01-25 17:04:10 +0800

    fix: fix font in Sonner toast

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,7 +14,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             className="toaster group"
             toastOptions={{
                 classNames: {
-                    toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+                    toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg group-[.toaster]:font-sans",
                     description: "group-[.toast]:text-muted-foreground",
                     actionButton:
                         "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
