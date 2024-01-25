f8536ddf6600e9932e3e5d5fd2099b5adef4c5ff
Jonathan Tay <jonathantaymy@gmail.com>
2024-01-25 17:03:42 +0800

    fix: fix sans font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ const config = {
         },
         extend: {
             fontFamily: {
-                sans: ["var(--font-satoshi)"],
+                sans: ["var(--font-satoshi)", "sans-serif"],
             },
             colors: {
                 border: "hsl(var(--border))",
